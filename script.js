6bb5d322b05e8627c3c15e376b0eb8ae99c68c43
Lexie Crawford <lexcraw4d@gmail.com>
2020-11-03 21:29:51 -0500

    question loop fixed

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 let timerEl= document.querySelector('#timer');
+let beginQuestionEl=document.querySelector('#beginQuestion')
 let startButton= document.querySelector('#startbtn');
 let correctMessage = document.querySelector('#correct-message');
 let questionChoices= document.querySelector('.choices');
 let questionContainerEl= document.getElementById('questioncontainer');
 let nextBtnEl= document.getElementById('nextBtn');
-let beginQuestionEl=document.getElementById('beginQuestion');
+let countdownEl= document.querySelector('#countdown');
+let score=0;
 let currentQuestionIndex = 0;
+let currentChoiceIndex =0;
 
-//Before Quiz Start
 nextBtnEl.style.visibility = 'collapse';
 
-//When the user starts the Quiz
+//Event listener for the start button
 startButton.addEventListener('click', function () {
 	startButton.style.visibility = 'collapse';
+	beginQuestionEl.style.visibility = 'collapse';
 	nextBtnEl.style.visibility = 'visible';
 	questionContainerEl.style.visibility = 'visible';
-	beginQuestionEl.style.visibility = 'collapse';
 
-	
-//Initial Timer Settings
-	let counter = 60;
-	let timerInterval = setInterval(function () {
+	// Timer
+	let counter= 60;
+	let timerInterval=setInterval(function () {
 		timerEl.textContent = counter + ' ' + 'seconds left';
 		counter--;
 		if (counter === -1) {
@@ -29,7 +30,6 @@ startButton.addEventListener('click', function () {
 		if (counter === -1) {
 			alert("Time's up! Let's see your scores.");
 		}
-		
 	}, 1000);
 	quizContent();
 });
@@ -38,44 +38,29 @@ function quizContent() {
 	let question = quizQuestions[currentQuestionIndex];
 	questionContainerEl.innerText = question.question;
 	for (let i=0;i<question.choices.length;i++){
-	var buttons = document.createElement("button");
+	let buttons = document.createElement("button");
 	buttons.textContent = question.choices[i];
 	questionChoices.append(buttons)}
 	buttons.addEventListener('click',checkAnswer());
 	
 	}
-
-
-
+//Checking answer against buttons
 function checkAnswer(answer) {
 	let question = quizQuestions[currentQuestionIndex];
 	if(answer === question.answer) {
-		correctMessage.innerText
+		correctMessage.innerText="CORRECT!"
 	}
-	if (answer === !question.answer){
-		counter=-10;
-	}
-	//next button?
 	currentQuestionIndex++;
 	quizContent();
-
 }
-//End game psuedo
-//make elements hide
-//create div for scores
-//save/initials
-//button to take to html highscore
-//create local storage for current score
-
-
-//High Scores
-//make score div hide
-//div highscore
-//retrieve from local storage
-//display previous scores in div
-
-
-
+//End Game
+//hide all other elements
+//clear div 
+//prompt user for initials
+//save to local storage
+//link highscore.html
+//pull saved highscores from storage
+//show past results
 //Questions
 var quizQuestions = [
 	{
@@ -106,3 +91,5 @@ var quizQuestions = [
 		answer: 'console.log'
 	},
 ];
+
+
